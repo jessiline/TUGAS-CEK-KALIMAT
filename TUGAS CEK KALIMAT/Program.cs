64b9df4aabf820318915a5ce95940ea09420f98a
jessiline <jimanuela@student.ciputra.ac.id>
2021-12-07 13:52:27 +0700

    Add project files.

---
+++ TUGAS CEK KALIMAT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+namespace TUGAS_CEK_KALIMAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Kalimat : ".ToUpper());
+            string kalimat = (Console.ReadLine()).ToUpper();
+            Console.Write("Input Kata : ");
+            string kata = Console.ReadLine();
+        }
+    }
+}
